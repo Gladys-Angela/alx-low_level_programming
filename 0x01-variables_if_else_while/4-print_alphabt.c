97d5f123493c690f28b638e97edfb058bafecb35
Gladys Angela <gladysangelakamau@gmail.com>
2023-06-16 14:01:33 -0700

    alphabet4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,22 @@
 #include <stdio.h>
+
+/**
+ * main - Prints the alphabet
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z')
+		{
+			if ((c != 'q' && c != 'e') && c <= 'z')
+				putchar(c);
+			c++;
+		}
+	putchar ('\n');
+	return (0);
+}
