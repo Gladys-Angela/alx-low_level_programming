3a4066c7e30cd0bed3e8b24782f6788bd4eba9fe
Gladys Angela <gladysangelakamau@gmail.com>
2023-06-16 14:15:44 -0700

    digits

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,1 +1,16 @@
 #include <stdio.h>
+
+/**
+ * main - Prints single digit numbers 10 base
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 10; a++)
+		putchar(a);
+	putchar ('\n');
+	return (0);
+}
