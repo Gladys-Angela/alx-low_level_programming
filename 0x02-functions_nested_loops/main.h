4db601caf4eee492dbe8d2a886a01c7648f1fc1e
Gladys Angela <gladysangelakamau@gmail.com>
2023-06-20 09:38:35 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+#ifndef main_h
+#define main_h
+
+/**
+ * Function prototypes 
+ */
+int myFunction(int a, int b);
+void anotherFunction();
+void someOtherFunction();
+
+/**
+ * Prototype for _putchar
+ */
+int _putchar(char c);
+
+#endif  /* MAIN_H */
+
